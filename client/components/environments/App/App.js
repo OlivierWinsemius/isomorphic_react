e33a085f0c46e79a6de2e5df4d3c900998298bea
Olivier Winsemius <o.winsemius@hotmail.com>
2018-11-10 18:26:11 +0100

    reverted App.js back to original state.

--- client/components/environments/App/App.js
+++ client/components/environments/App/App.js
@@ -1,13 +1,4 @@
-import { useState } from 'react';
+import React from 'react';
+import Routes from '../../ecosystems/Routes';
 
-export default function Example() {
-    // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
-
-    return (
-        <div>
-            <span>You clicked {count} times</span>
-            <button onClick={() => setCount(count + 1)}>Click me</button>
-        </div>
-    );
-}
+export default () => <Routes />;
