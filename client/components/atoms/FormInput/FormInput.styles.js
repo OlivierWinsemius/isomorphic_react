cb4531f76579b34775352b20aa24522d011b3325
Olivier Winsemius <o.winsemius@hotmail.com>
2018-11-11 19:42:53 +0100

    updating form styling.

--- client/components/atoms/FormInput/FormInput.styles.js
+++ client/components/atoms/FormInput/FormInput.styles.js
@@ -1,4 +1,14 @@
 export default () => ({
-    label: {},
-    input: {},
+    label: {
+        width: '100%',
+        flex: 1,
+        padding: 6,
+        textAlign: 'right',
+    },
+    input: {
+        flex: 3,
+    },
+    inputWrapper: {
+        display: 'flex',
+    },
 });
---
+++ client/components/molecules/Form/Fields/Field.styles.js
@@ -0,0 +1,8 @@
+export default {
+    field: {
+        margin: '6px 0',
+        border: 'none',
+        padding: 0,
+        width: '100%',
+    },
+};
